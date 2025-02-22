321417ec75cc77c6c1c48e1989a39c75310e1de6
sgbtech <sgbtech62@gmail.com>
2025-02-22 14:44:40 +0000

    Updating lambda txt

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -3,7 +3,7 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': jsin.dumps("Hello from CICD Lambda")
+        'body': jsin.dumps("Hello from our CICD github actions workflow using VScode")
 
     }
 
