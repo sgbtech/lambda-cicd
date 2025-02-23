c60743a57af298b7fd6d099f4809055081336814
sgbtech <sgbtech62@gmail.com>
2025-02-23 14:34:59 +0000

    Updates

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -3,7 +3,8 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps("Hello from Lambda")
+        'body': json.dumps("Hello from Lambda from vscode")
+        
 
     }
 
