602183a6e9b166b3ab564204afe64abcd6dac178
sgbtech <sgbtech62@gmail.com>
2025-02-23 16:00:05 +0000

    Updates

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -3,8 +3,8 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps("Hello from Lambda Today")
-        
+        'body': json.dumps("Hello from CICD Lambda")
+
 
 
 
